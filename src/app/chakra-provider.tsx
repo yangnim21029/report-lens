27bce75ee82924bd72ae6088b607d98c986154ec
yangnim21029 <yangnim21029@gmail.com>
2025-11-29 00:31:27 +0800

    remove to slim

--- src/app/chakra-provider.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { ChakraProvider } from "@chakra-ui/react";
-import { system } from "~/styles/theme";
-
-// 只在需要 Chakra UI 組件的地方使用這個 Provider
-export function ChakraWrapper({ children }: { children: React.ReactNode }) {
-	return <ChakraProvider value={system}>{children}</ChakraProvider>;
-}
